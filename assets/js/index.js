144850ab87d4f67e558444834c91fb81e63f0226
Nguyễn Văn Viên <nguyenvien161002@gmail.com>
2022-05-23 02:35:01 +0700

    fix change modal login,register and insert font 'roboto'

--- assets/js/index.js
+++ assets/js/index.js
@@ -86,3 +86,19 @@ modalSiderBar.addEventListener('click', function(event){
 btnCart.addEventListener('click', showModalCart);
 btnClose.addEventListener('click', hiddenModal);
 btnSiderBar.addEventListener('click', showSidebar);
+
+// LOGIN WITH GG 
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+}
