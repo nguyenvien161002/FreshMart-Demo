97396a10bcd9628500ef4bd08b062f0d48c0e3a3
Nguyễn Văn Viên <nguyenvien161002@gmail.com>
2022-06-05 17:01:35 +0700

    fix and add item to cart using local storage

--- assets/js/addproduct.js
+++ assets/js/addproduct.js
@@ -1,120 +1,149 @@
-// ADD PRODUCT
+var products = [
+    {
+        id: 1,
+        name: 'Ổi lê ruột đỏ',
+        price: 20,
+        quantity: 1,
+        weight: 1,
+        imgSRC: '/assets/images/sp1.webp'
+    },
+    {
+        id: 2,
+        name: 'Đào đỏ Mỹ',
+        price: 40,
+        quantity: 1,
+        weight: 1,
+        imgSRC: '/assets/images/sp2.webp'
+    },
+    {
+        id: 3,
+        name: 'Dâu tây',
+        price: 138,
+        quantity: 1,
+        weight: 1,
+        imgSRC: '/assets/images/sp3.webp'
+    },
+    {
+        id: 4,
+        name: 'Chanh tươi vỏ xanh',
+        price: 30,
+        quantity: 1,
+        weight: 1,
+        imgSRC: '/assets/images/sp4.webp'
+    },
+    {
+        id: 5,
+        name: 'Trái cam mật',
+        price: 138,
+        quantity: 1,
+        weight: 1,
+        imgSRC: '/assets/images/sp5.webp'
+    },
+];
+
 const btnAdd = document.querySelector('.js--btnaddproduct');
-var imgProduct = document.querySelector('.imgproduct').src;
-var nameProduct = document.querySelector('.js-nameproduct').innerText;
-var priceProduct = document.querySelector('.js--price').innerText;
-var weightProduct = document.querySelector('.js--btnweight1').innerText;
-var containproducts = document.querySelector('.js--modal__cartshpping');
-var nocontainproduct = document.querySelector('.js--noproduct');
-var paymentMoney = document.querySelector('.js--summoney');
-var intoMoney = document.querySelector('.js--intomoney');
-var notification = document.querySelector('.js--notification');
-var arrayContainPro = [];
-var count = 0;
-var infoproductOj = {
-    name: nameProduct,
-    price: priceProduct,
-    weight: weightProduct
-}
-function addProduct(){
-    nocontainproduct.classList.add('hidden');
-    var addModalcontent = document.createElement('div');
-    count++;
-    addModalcontent.innerHTML = 
-        '<div class="modal__cartshopping--content">'
-            +'<div class="imgproduct">'
-                    +'<img src="'+imgProduct+'" alt="" class="imgproduct--img">'
-            +'</div>'
-            +'<div class="infoproduct">'
-                +'<div class="infoproduct_boxweight">'
-                    +'<span class="infoproduct--name">'+nameProduct+'</span>'
-                    +'<span class="infoproduct--separate">-</span>'
-                    +'<p class="infoproduct_boxweight--weight">'+weightProduct+'</p>'
-                +'</div>'
-                +'<div class="infoproduct__button">'
-                    +'<label for="">Số lượng: </label>'
-                    +'<button class="infoproduct__button--increase">-</button>'
-                    +'<input class="quantity" value="'+intQuantity+'"></input>'
-                    +'<button class="infoproduct__button--decrease">+</button>'
-                +'</div>'
-                +'<div class="infoproduct__priceproduct">'
-                    +'<p class="priceproduct--labelprice">Giá: </p>' 
-                    +'<span class="priceproduct--price">'+priceProduct+'</span>'
-                +'</div>'
-                +'<button class="infoproduct__btnromove">Bỏ sản phẩm</button>'
-            +'</div>'
-        +'</div>';
-    containproducts.append(addModalcontent);
-    paymentMoney.classList.add('show');
-    arrayContainPro.push(infoproductOj);
-    var sum = 0;
-    function totalpro(){
-        for(var key of arrayContainPro){
-            sum = parseInt(sum) + parseInt(key.price)*intQuantity;
-        }
-        return sum;
-    }
-    var sumP = String(totalpro());
-    intoMoney.innerText = sumP;
-    notification.innerText = count;
-    notification.classList.add('opacity');
+const nameProduct = document.querySelector('.js-nameproduct');
+const productID = parseInt(nameProduct.id);
+const containerCartItem = document.querySelector('.js--container__cartitem');
+const noCartItem = document.querySelector('.js--noproduct');
+const payment = document.querySelector('.js--summoney');
+const notiCart = document.querySelector('.js--cartnotification');
+const intoMoney = document.querySelector('.js__intomoney');
 
-    var btnRomove = addModalcontent.querySelector('.infoproduct__btnromove');
-    btnRomove.addEventListener('click', function(){
-        count--;
-        addModalcontent.remove();
-        notification.innerText = count;
-        arrayContainPro.pop();
-        sum = 0;
-        intoMoney.innerText = String(totalpro());
-        if(notification.innerText==0){
-            paymentMoney.classList.remove('show');
-            nocontainproduct.classList.remove('hidden');
-            notification.classList.remove('opacity');
-        }
-    })
-}
+let cart = JSON.parse(localStorage.getItem('CART')) || [];
+updateCart();
 
-// Increase or decrease the amount
-var btnMinus = document.querySelector('.js--minus');
-var btnPlus = document.querySelector('.js--plus');
-var valueQuantity = document.querySelector('.js--enterquantity');
-var intQuantity = parseInt(valueQuantity.value);
-
-btnMinus.addEventListener('click', function(){
-    if(intQuantity==0){
-        return
-    } else {
-        intQuantity--;
-        valueQuantity.value= intQuantity;
-    }
-})
-
-btnPlus.addEventListener('click', function(){
-    intQuantity++;
-    valueQuantity.value= intQuantity;
-})
-
-btnAdd.addEventListener('click', addProduct);
-
-// Images Gallery
-var images = document.querySelectorAll('.js--imagegallery');
-var imgproduct = document.querySelector('.js--imgproduct img');
-var imagesz = document.getElementsByClassName('js--imagegallery');
-
-var currentIndex = 0;
-
-images[currentIndex].classList.add('opacity');
-
-images.forEach(function(item, index){
-    item.addEventListener('click', function(){
-        currentIndex = index;
-        images.forEach(function(item, index){
-            item.classList.remove('opacity');
-        })
-        imgproduct.src = images[currentIndex].src;
-        images[currentIndex].classList.add('opacity');
-    })
+btnAdd && btnAdd.addEventListener('click', function(){
+    addToCart(productID);
 });
+
+function addToCart(id) {
+    if(cart.some(function(item){return item.id === id})){
+        alert('Exsits product!');
+    } else {
+        const item = products.find(function(product){
+            return product.id === id;
+        });
+        cart.push(item)
+    }
+    updateCart();
+}
+
+function updateCart() {
+    renderCartItem();
+    localStorage.setItem('CART', JSON.stringify(cart));
+    notiCart.innerText = cart.length; 
+    if(cart.length==0){
+        noCartItem.classList.remove('hidden');
+        payment.classList.remove('show');
+    }
+    totalMoneyToCart();
+}
+
+function totalMoneyToCart() {
+    var sum = 0;
+    cart.forEach(function(item){
+        sum = sum + parseInt(item.price)*item.quantity;
+        intoMoney.innerText = sum;
+    })
+};
+
+function renderCartItem(){
+    containerCartItem.innerHTML = "";
+    noCartItem.classList.add('hidden');
+    cart.forEach(function(item, index){
+        containerCartItem.innerHTML += `
+            <div class="modal__cartshopping--content">
+                <div class="imgproduct">
+                        <img src="${item.imgSRC}" alt="" class="imgproduct--img">
+                </div>
+                <div class="infoproduct">
+                    <div class="infoproduct_boxweight">
+                        <span class="infoproduct--name">${item.name}</span>
+                        <span class="infoproduct--separate">-</span>
+                        <p class="infoproduct_boxweight--weight">${item.weight}Kg</p>
+                    </div>
+                    <div class="infoproduct__button">
+                        <label for="">Số lượng: </label>
+                        <button class="infoproduct__button--decrease" onclick="changeNumberOfUnits('minus', ${item.id})">-</button>
+                        <input class="quantity js__quantity" value="${item.quantity}"></input>
+                        <button class="infoproduct__button--increase" onclick="changeNumberOfUnits('plus', ${item.id})">+</button>
+                    </div>
+                    <div class="infoproduct__priceproduct">
+                        <p class="priceproduct--labelprice">Giá: </p> 
+                        <span class="priceproduct--price">${item.price}.000đ</span>
+                    </div>
+                    <button class="infoproduct__btnromove btn__removeItemCart" data-id="${index}">Bỏ sản phẩm</button>
+                </div>
+            </div>
+        `
+    })
+    payment.classList.add('show'); 
+    var btnsRemoveItemCart = document.querySelectorAll('.btn__removeItemCart');
+    btnsRemoveItemCart.forEach((itemBtnRemove,index) => {
+        itemBtnRemove.addEventListener('click', function(){
+            cart.splice(index, 1);
+            console.log(itemBtnRemove.getAttribute('data-id'));
+            updateCart();
+        })
+    })
+};
+
+// Không thể để Logic xóa 1 sản phẩm của cart bên ngoài hàm renderCartItem(), 
+// bời vì mỗi lần render lại cart item thì các btnRomove sẽ được tạo mới trong cây DOM 
+// tại thời điểm này file js này đã được thực thi nên các btnRomove lúc này sẽ ko có clickEvent
+// =>> không xóa đc sản phẩm trong cart nx.
+
+// var btnsRemoveItemCart = document.querySelectorAll('.btn__removeItemCart');  
+// btnsRemoveItemCart.forEach((itemBtn,index) => {
+//     itemBtn.addEventListener('click', function(){
+//         cart.splice(index, 1);
+//         console.log(itemBtn.getAttribute('data-id'));
+//         updateCart();
+//     })
+// })
+
+
+
 
 
